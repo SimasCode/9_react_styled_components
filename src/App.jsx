3d9b7de8c315a9cae0d7bae153e2977cbda983db
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-17 08:57:57 +0300

    react styled components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const MainTitle = styled.h1`
+  color: tomato;
+  font-weight: normal;
+  text-transform: uppercase;
+`;
+
+export default function App() {
+  return (
+    <div>
+      <MainTitle>Styled components</MainTitle>
+    </div>
+  );
+}
